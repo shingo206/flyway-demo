45740958cc4da0feb671d49d8a6716b43438aae1
Shingo Tamura <shingo206@mac.com>
2020-10-01 20:58:14 +0200

    Add @Repository annotation for 2 repositories.

--- src/main/java/com/example/flywaydemo/repository/CustomerRepository.java
+++ src/main/java/com/example/flywaydemo/repository/CustomerRepository.java
@@ -4,9 +4,10 @@ import com.example.flywaydemo.dto.OrderResponse;
 import com.example.flywaydemo.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     @Query("select new com.example.flywaydemo.dto.OrderResponse(c.name, p.productName) from Customer c join c.products p")
     List<OrderResponse> getJoinInformation();
--- src/main/java/com/example/flywaydemo/repository/ProductRepository.java
+++ src/main/java/com/example/flywaydemo/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.example.flywaydemo.repository;
 
 import com.example.flywaydemo.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
